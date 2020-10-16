c090b9607e356901a073edfa06a996b4c5e0ef47
bl4cksugar <n.jarzmicka@wp.pl>
2020-10-16 09:09:52 +0200

    basic linter, prettier config. Cleaning vue template

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
+import Axios from "axios";
 import Vue from "vue";
+import VueAxios from "vue-axios";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+Vue.use(VueAxios, Axios);
+
+Vue.axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: (h) => h(App)
 }).$mount("#app");
