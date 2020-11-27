3d5682dba833e2f6ec5eb5cbe5e5f236b7d129da
bboysokol <bboy_sokol@o2.pl>
2020-11-27 15:48:29 +0100

    cookie handling improved, game code input improved

--- src/helpers/cookieHelper.js
+++ src/helpers/cookieHelper.js
@@ -1,6 +1,8 @@
 import cookie from "vue-cookies";
 
 const SESSION_COOKIE_NAME = "common_token";
+const APP_DOMAIN =
+	process.env.VUE_APP_NODE_ENV === "prod" ? "judge-em.github.io" : "";
 
 const defaultParams = {
 	expires: "2h"
@@ -8,7 +10,7 @@ const defaultParams = {
 
 export default {
 	deleteSessionCookie() {
-		cookie.remove(SESSION_COOKIE_NAME, "/webapp", "judge-em.github.io");
+		cookie.remove(SESSION_COOKIE_NAME, "/webapp", APP_DOMAIN);
 	},
 	hasSessionCookie() {
 		return !!cookie.get(SESSION_COOKIE_NAME);
@@ -19,7 +21,7 @@ export default {
 			token,
 			defaultParams.expires,
 			"/webapp",
-			"judge-em.github.io"
+			APP_DOMAIN
 		);
 	},
 	getSessionCookie() {
--- src/views/Home/content/RoomCard.vue
+++ src/views/Home/content/RoomCard.vue
@@ -18,12 +18,13 @@
 								<ValidationProvider
 									ref="roomCode"
 									name="RoomCode"
-									rules="required|max:6|min:6"
+									rules="required|min:6"
 									v-slot="{ errors }"
 								>
 									<el-input
 										:placeholder="$t('home.RoomCode')"
 										v-model="roomCode"
+										maxlength="6"
 										:class="[{ 'is-invalid': errors[0] }]"
 									>
 										<i
